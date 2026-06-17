b22bcb4992eb91b4fcdd078b41709b80ccaa7ee5
codebyharsh01 <ht155794@gmail.com>
2026-06-17 13:28:17 +0530

    Add files via upload

---
+++ day 13/50.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    
+    int arr[n];
+    int sum = 0;
+    cout << "Enter " << n << " elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    
+    double avg = (double)sum / n;
+    cout << "Sum: " << sum << endl;
+    cout << "Average: " << avg << endl;
+    return 0;
+}
---
+++ day 13/52.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    
+    int arr[n];
+    int evenCount = 0, oddCount = 0;
+    
+    cout << "Enter " << n << " elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if(arr[i] % 2 == 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+    }
+    
+    cout << "Number of even elements: " << evenCount << endl;
+    cout << "Number of odd elements: " << oddCount << endl;
+    return 0;
+}
